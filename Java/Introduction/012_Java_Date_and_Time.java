9867d5b66591dff238ba7c9ab214002096cbc025
felipefrizeiro@gmail.com <felipefrizeiro@gmail.com>
2016-10-10 17:25:11 -0300

    Java Date and Time

---
+++ Java/Introduction/012_Java_Date_and_Time.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int month = in.nextInt();
+		int day = in.nextInt();
+		int year = in.nextInt();
+
+		String dateString = String.format("%d-%d-%d", year, month, day);
+		String dayOfWeekName = "";
+
+		try {
+			Date date = new SimpleDateFormat("yyyy-M-d").parse(dateString);
+			dayOfWeekName = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(date);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		System.out.println(dayOfWeekName.toUpperCase());
+
+	}
+}
