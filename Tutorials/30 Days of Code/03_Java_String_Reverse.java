99c35bef15b947e8b57f54332fff4ee45101bcff
felipefrizeiro@gmail.com <felipefrizeiro@gmail.com>
2016-10-13 11:09:02 -0300

    Java > Strings > Java String Reverse

---
+++ Tutorials/30 Days of Code/03_Java_String_Reverse.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+
+		/* Enter your code here. Print output to STDOUT. */
+		System.out.println(isPalindrome(A) ? "Yes" : "No");
+
+	}
+
+	public static boolean isPalindrome(String str) {
+		return str.equals(new StringBuilder(str).reverse().toString());
+	}
+	
+}
