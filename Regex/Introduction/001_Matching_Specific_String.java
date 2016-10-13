ffaaed8d429be82a05515242c1ba67d0bf0612d0
frizeiro <felipefrizeiro@gmail.com>
2016-10-13 10:45:46 -0300

    Regex - Introduction - Matching Specific String

--- Regex/Introduction/001_Matching_Specific_String.java
+++ Regex/Introduction/001_Matching_Specific_String.java
@@ -7,9 +7,7 @@ import java.util.regex.*;
 public class Solution {    
 
     public static void main(String[] args) {
-        
         Regex_Test tester = new Regex_Test();
         tester.checker("hackerrank"); 
-    
     }
 }
