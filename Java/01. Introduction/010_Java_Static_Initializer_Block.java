fd83f6abc46e3be63d654aeb65a89d589bb951f2
frizeiro <felipefrizeiro@gmail.com>
2016-10-13 10:50:48 -0300

    Java > Introduction > Java Static Initializer Block

--- Java/01. Introduction/010_Java_Static_Initializer_Block.java
+++ Java/01. Introduction/010_Java_Static_Initializer_Block.java
@@ -10,6 +10,7 @@ public class Solution {
 	static boolean flag;
 
 	static {
+		
 		Scanner sc = new Scanner(System.in);
 		B = sc.nextInt();
 		H = sc.nextInt();
@@ -19,13 +20,16 @@ public class Solution {
 		} else {
 			System.out.println("java.lang.Exception: Breadth and height must be positive");
 		}
+		
 	}
 
 	public static void main(String[] args) {
+		
 		if (flag) {
 			int area = B * H;
 			System.out.print(area);
 		}
+		
 	}
 
 }
